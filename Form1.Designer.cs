b1228f3766707c41aeacaf67a6be84c151dd5a63
reyda <reyda@LAPTOP-ALH488S6>
2023-11-27 00:48:39 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace U3_P2_Practica_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 23);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(645, 27);
+            label1.TabIndex = 0;
+            label1.Text = "Imprime numeros de un rango específico y calcula la suma\r\n";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Rockwell", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(21, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(165, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Rango Inferior:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Rockwell", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(335, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(177, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Rango Superior:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 11F, FontStyle.Italic, GraphicsUnit.Point);
+            textBox1.Location = new Point(192, 108);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(137, 37);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 11F, FontStyle.Italic, GraphicsUnit.Point);
+            textBox2.Location = new Point(518, 108);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(137, 37);
+            textBox2.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 27;
+            listBox1.Location = new Point(49, 172);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(274, 274);
+            listBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Aquamarine;
+            button1.Font = new Font("Stencil", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(401, 216);
+            button1.Name = "button1";
+            button1.Size = new Size(195, 34);
+            button1.TabIndex = 6;
+            button1.Text = "Imprime";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Rockwell", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(351, 327);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 25);
+            label4.TabIndex = 7;
+            label4.Text = "Suma:";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 11F, FontStyle.Italic, GraphicsUnit.Point);
+            textBox3.Location = new Point(430, 320);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(214, 37);
+            textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 27F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleTurquoise;
+            ClientSize = new Size(680, 486);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Rockwell", 12F, FontStyle.Italic, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Suma especifica";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private Button button1;
+        private Label label4;
+        private TextBox textBox3;
+    }
+}
